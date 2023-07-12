362e02b9782885ef0d3e36eb0486356b51a5213f
Minakshi Karwa <58382167+Minakshi85@users.noreply.github.com>
2023-07-12 13:20:29 -0400

    Update SQLAnkitBansalQuestions.sql

--- SQLAnkitBansalQuestions.sql
+++ SQLAnkitBansalQuestions.sql
@@ -313,4 +313,38 @@ with cte as
 
 Select * from cte where rn =2 or cnt =1
 /***************************************************************************************************************************************************************************************************************************/
+/* Sportify Case Study */
+CREATE table activity
+(
+user_id varchar(20),
+event_name varchar(20),
+event_date date,
+country varchar(20)
+);
+insert into activity values (1,'app-installed','2022-01-01','India'),(1,'app-purchase','2022-01-02','India'),(2,'app-installed','2022-01-01','USA'),(3,'app-installed','2022-01-01','USA'),(3,'app-purchase','2022-01-03','USA')
+,(4,'app-installed','2022-01-03','India'),(4,'app-purchase','2022-01-03','India'),(5,'app-installed','2022-01-03','SL'),(5,'app-purchase','2022-01-03','SL'),(6,'app-installed','2022-01-04','Pakistan'),(6,'app-purchase','2022-01-04','Pakistan');
 
+/* Question 1 : Find total active users daily */
+select event_date, count(distinct user_id) as active_user from activity group by event_date
+
+ /* Question 2 : Total active users weekly */
+Select datepart(week, event_date) as week, count(distinct user_id) as active_user from activity group by datepart(week, event_date)
+
+ /*Question 3 : date-wise total number of users who purchased the app same day*/
+with users as
+(
+select user_id, event_date, CASE WHEN count(distinct event_name) = 2 Then user_id else NULL END as user from activity group by user_id, event_date
+)
+
+select event_date, count(user) from users group by event_date
+/*Question 4 : percentage of paid users in India, USA and Other countries */
+with cte as
+(
+select 
+CASE WHEN Country in('India','USA')Then country else 'Others' END as new_country, count(distinct user_id) as paid from activity
+where event_name = 'app-purchase' group by CASE WHEN Country in('India','USA')Then country else 'Others' END
+),
+total as(Select  sum(paid) as total from cte)
+Select new_country, (1.0*(paid)/total)*100 as paid_users from cte, total
+
+/*Question 5 : find the number of users who made same day purchases */
